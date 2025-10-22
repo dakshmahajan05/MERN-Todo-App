c10f3aed654f736b2a298ee4d8affd4a3f6a9811
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-10-22 21:20:43 +0530

    wrote user auth controllers

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,10 +1,55 @@
-const loginUser = async ()=>{
+import User from "../models/user.model";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
+const registerUser = async(req,res)=>{
+   try {
+
+
+     const {email,username,password} = req.body;
+
+     const user =await User.findOne({email})
+     if(user){
+        return res.status(400).json({message:"user already exist"})
+     }
+
+     const hashPass = bcrypt.hashSync(password,10)
+
+     const newUser =await new User({email,username,password:hashPass});
+     await newUser.save();
+
+     return res.status(200).json({message: "user registered successfully", newUser})
+   } catch (error) {
+    console.log("err while registering user",error.message);
+    return res.status(400).json("err while registering user")
+   }
+}
+const loginUser = async (req,res)=>{
+   try {
+       const {email,password} = req.body;
+       const user =await User.findOne({email})
+       if(!user){
+         return res.status(400).json({message:"user not found"})
+       }
+
+       const passCorrect =await bcrypt.compare(password,user.password)
+       if(passCorrect){
+          const token = jwt.sign({email},process.env.JWT_SECRET,{expiresIn:'1d'})
+          console.log("login successfull");
+         return res.status(200).json({message:"user login successfull",token,user})
+       }else{
+         return res.status(400).json({message:"invalid credentials"})
+       }
+   } catch (error) {
+      console.log("err while login");
+      return res.status(400).json({message:"unable to login"})
+      
+   }
+    
 }
 
-const registerUser = async()=>{
-
-}
 
 const usercontrollers = {
     loginUser,
