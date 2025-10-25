37bf7855289b7057c6fe955174831b72badeed45
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-10-25 22:16:28 +0530

    wrote axios file in frontend and authcontext

---
+++ frontend/src/Context/AuthContext.js
@@ -0,0 +1,53 @@
+import { createContext } from "react";
+import API from "../api/axios";
+import { useState } from "react";
+
+export const AuthContext = createContext()
+
+const AuthContextProvider = (props)=>{
+
+    const [user,setuser]= useState(null)
+
+    const register = async(formdata)=>{
+        try {
+                const res = await API.post('/user/register',formdata)
+                console.log("register response:",res.data);
+
+        } catch (error) {
+                console.log("err.response.data");
+
+                
+        }
+    
+    }
+
+    const login = async(formdata)=>{
+        try {
+            const res = API.post('/user/login',formdata)
+            console.log("register response: ",res.data);
+            setuser(res.data.user)
+            localStorage.setItem("token",(await res).data.token)
+            return res.data
+            
+        } catch (error) {
+            console.log("err.response.data");
+            
+        }
+    }   
+
+
+    const logout = ()=>{
+        setuser(null)
+        localStorage.removeItem('token')
+    }
+
+
+
+   return (
+     <AuthContext.Provider value={{logout,register,login,user}}>
+        {props.children}
+    </AuthContext.Provider>
+   )
+}
+
+export default AuthContextProvider;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import AuthContextProvider from './Context/AuthContext.js'
 createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
+    <AuthContextProvider>
+        <BrowserRouter>
     <App />
     </BrowserRouter>
+    </AuthContextProvider>
   
 )
